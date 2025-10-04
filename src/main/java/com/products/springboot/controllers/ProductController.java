970e62a30bd91dd4e8c0b476da73a4da0d2557c9
Marcelo da Silva <marceloxhenrique@hotmail.com>
2025-10-04 23:27:19 +0200

    feat: Add Spring HATEOS support for products endpoints - Add self and list links to ProductController GET endpoints (/products and /products/{id})

--- src/main/java/com/products/springboot/controllers/ProductController.java
+++ src/main/java/com/products/springboot/controllers/ProductController.java
@@ -14,6 +14,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 public class ProductController {
     @Autowired
@@ -28,7 +31,14 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts(){
-        return  ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+        List<ProductModel> productList = productRepository.findAll();
+        if(!productList.isEmpty()){
+            for(ProductModel product : productList){
+                UUID id = product.getIdProduct();
+                product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
+        return  ResponseEntity.status(HttpStatus.OK).body(productList);
     }
 
     @GetMapping("/products/{id}")
@@ -38,6 +48,7 @@ public class ProductController {
             System.out.println("Here not found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
+        product.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Product List"));
         return ResponseEntity.status(HttpStatus.OK).body(product.get());
     }
 
